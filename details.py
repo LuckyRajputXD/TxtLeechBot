e451c27cb2872e1584d5289d9d856ccf8ec1bc15
Gobotmade <129635610+Gobotmade@users.noreply.github.com>
2023-05-02 16:06:24 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 26435700
 api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
+bot_token = "6171627721:AAG4r1VPjLe0ipLM-rxAWgc80ubQTIzi2qY"
 
